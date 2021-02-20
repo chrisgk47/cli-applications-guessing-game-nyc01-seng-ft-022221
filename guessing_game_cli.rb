913818cac7e4a2f76da54b2a9d445c717e6d2c09
Christopher Kim <chris.kim1990@gmail.com>
2021-02-20 19:13:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif input == num
     puts "You guessed the correct number!"
+    num
   elsif input != num
     puts "Sorry! The computer guessed #{num}."
   else
